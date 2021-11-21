f4616901563029ac21283b2652e9b8f680d927ac
rafapeterson <rafaaapeterson@gmail.com>
2021-11-21 07:55:19 -0700

    added to app.js

--- static/js/app.js
+++ static/js/app.js
@@ -20,4 +20,25 @@ function buildTable(data) {
       cell.text(val);
     });
   });
-}
+};
+
+function handleClick(){
+    // telling D3 to look for the #datetime ID in the HTML tags & having it hold the data in the "date" variable.
+   let date = d3.select("#datetime").property("value");
+   let filteredData = tableData;
+
+   //using if statement to check for date & if present return only the data with that date.
+   if (date) {
+       // apply 'filter' to the table data to only keep the rows where 'datetime' value matches the filter value
+       filteredData = filteredData.filter(row => row.datetime === date);
+   };
+
+   // Rebuild the table using filtered data
+   buildTable(filteredData);
+
+};
+// attach an event to listen for the form button
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// build the table when the page loads
+buildTable(tableData);
